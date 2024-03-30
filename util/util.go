236e66a31e14fe7a80653e377804cb24b9a57360
sheeiavellie <josephlandmars@gmail.com>
2024-03-30 19:28:56 +0300

    feat: add ReadJSON

--- util/util.go
+++ util/util.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+func ReadJSON(r *http.Request, v any) error {
+	return json.NewDecoder(r.Body).Decode(v)
+}
+
 func WriteJSON(w http.ResponseWriter, status int, v any) error {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(status)
