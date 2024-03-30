cfb0668aa5cc00dd15ee12d825bb38daafc3c9c1
sheeiavellie <josephlandmars@gmail.com>
2024-03-30 16:01:07 +0300

    feat: add bson unmarshaller to User

--- data/types.go
+++ data/types.go
@@ -1,14 +1,42 @@
 package data
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 type User struct {
-	ID     uuid.UUID
-	IsCool bool
+	ID     uuid.UUID `bson:"user_id"`
+	IsCool bool      `bson:"is_cool"`
+}
+
+func (u *User) UnmarshalBSON(data []byte) error {
+	var raw bson.M
+	if err := bson.Unmarshal(data, &raw); err != nil {
+		return err
+	}
+
+	idStr, ok := raw["user_id"].(string)
+	if !ok {
+		return fmt.Errorf("user_id field not found or not a string")
+	}
+	id, err := uuid.Parse(idStr)
+	if err != nil {
+		return err
+	}
+
+	isCool, ok := raw["is_cool"].(bool)
+	if !ok {
+		return fmt.Errorf("is_cool field not found or not a boolean")
+	}
+
+	u.ID = id
+	u.IsCool = isCool
+
+	return nil
 }
 
 type RefreshSession struct {
