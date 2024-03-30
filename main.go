40f844761dd94f2d709723fd2e45fae065da30d5
sheeiavellie <josephlandmars@gmail.com>
2024-03-30 16:02:03 +0300

    add sing-in middleware

--- main.go
+++ main.go
@@ -7,33 +7,42 @@ import (
 
 	"github.com/sheeiavellie/medods290324/handlers"
 	"github.com/sheeiavellie/medods290324/middlewares"
+	"github.com/sheeiavellie/medods290324/services"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	port := "1337"
 	ctx := context.Background()
 
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:admin@localhost:27017"))
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	tokenService := services.NewTokenService([]byte("secret"))
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc(
 		"POST /sing-in",
-		middlewares.ValidateUser(
+		middlewares.SingIn(
 			ctx,
-			middlewares.SingIn(
-				ctx,
-				handlers.HandleSingIn,
-			),
+			client,
+			tokenService,
+			handlers.HandleSingIn,
 		),
 	)
 
 	mux.HandleFunc(
 		"POST /refresh",
-		middlewares.ValidateRefresh(
+		middlewares.Refresh(
 			ctx,
-			middlewares.Refresh(
-				ctx,
-				handlers.HandleRefresh,
-			),
+			client,
+			handlers.HandleRefresh,
 		),
 	)
 
--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -2,38 +2,57 @@ package middlewares
 
 import (
 	"context"
+	"errors"
+	"log"
 	"net/http"
+
+	"github.com/sheeiavellie/medods290324/data"
+	"github.com/sheeiavellie/medods290324/services"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
-
-func ValidateUser(
-	ctx context.Context,
-	next http.HandlerFunc,
-) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		next(w, r)
-	}
-}
-
-func ValidateRefresh(
-	ctx context.Context,
-	next http.HandlerFunc,
-) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		next(w, r)
-	}
-}
 
 func SingIn(
 	ctx context.Context,
+	db *mongo.Client,
+	tokenService *services.TokenService,
 	next http.HandlerFunc,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		userIDStr := r.URL.Query().Get("userID")
+		if userIDStr == "" {
+			log.Printf("userID was not provided.")
+			http.Error(w, "userID was not provided", http.StatusBadRequest)
+			return
+		}
+
+		usersCollection := db.Database("amogus").Collection("users")
+
+		var user *data.User
+		err := usersCollection.FindOne(
+			context.TODO(),
+			bson.M{"user_id": userIDStr},
+		).Decode(&user)
+		if err != nil {
+			switch {
+			case errors.Is(err, mongo.ErrNoDocuments):
+				log.Println("no document :(")
+			default:
+				log.Printf("error getting user: %v.", err)
+			}
+			return
+		}
+
+		// check if user exists in db
+		// if he does, than issue tokens
+		// then create refresh session
 		next(w, r)
 	}
 }
 
 func Refresh(
 	ctx context.Context,
+	db *mongo.Client,
 	next http.HandlerFunc,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
